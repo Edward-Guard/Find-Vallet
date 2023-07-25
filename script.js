bf428c10c8426f9c648a26f722e09cecebaeb1e9
Edward Silva <107967519+Edward-Guard@users.noreply.github.com>
2023-07-24 22:51:47 -0300

    Estilizando

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+
 const div = document.getElementById("container");
 const btnChange = document.getElementById("change");
+const btnShow = document.getElementById("show");
 
 div.addEventListener('click', virar)
+div.addEventListener('click', choice)
 btnChange.addEventListener('click', iniciando)
+btnShow.addEventListener('click', showAll)
 
 const positions = [201.6,403.2,604.8,0]
 
@@ -10,19 +14,42 @@ function changePositions(params) {
     positions.sort(() => Math.random() - 0.5);
     let elementos = div.children
     for (let i = 0; i < elementos.length; i +=1) {
-        console.log(elementos[i]);
         elementos[i].style.left = `${positions[i]}px`
     }
 }
 
-function iniciando() {
-    for (let i = 0; i < 8; i++) {
-        setTimeout(() => changePositions(), 1000 * i);
+function virar(e) {
+    const pai = e.target.parentElement
+    pai.classList.toggle('flipada')
+    
+}
+
+//Dxar pronta
+function showAll(e) {
+    for (let i = 0; i < 4; i += 1) {
+        if (div.children[i].classList.toggle('flipada')) {
+            div.children[i].classList.toggle('flipada')
+        }
+        div.children[i].classList.toggle('flipada')
+    }  
+}
+
+function choice(e) {
+    const escolhido = document.querySelector('.escolhido')
+    if (escolhido) {
+        escolhido.classList.toggle('escolhido')
+    }
+    e.target.classList.toggle('escolhido');
+    if (e.target.id == 'paladin') {
+        console.log('Você acertou');
     }
 }
 
-function virar(e) {
-    e.target.classList.toggle('flipada');
+function iniciando() {
+    showAll()
+    for (let i = 0; i < 8; i++) {
+        setTimeout(() => changePositions(), 1000 * i);
+    }
 }
 
 //Criar container perspective -> Carta{Frente / Trás}
